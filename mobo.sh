f859914b6408e496d0e6c636e11ef143d20774b5
nitamigina <61388805+nitamigina@users.noreply.github.com>
2022-04-25 06:59:59 +0000

    Add files via upload

---
+++ mobo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-ru.rplant.xyz   /Moscow/
+#       stratum-eu.rplant.xyz   /London/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Toronto/
+#
+FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+while [ 1 ]; do
+"$FOLDER"/cpuminer-sse2 -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u WALLET.WORKER_NAME
+sleep 5
+done
