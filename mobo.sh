443619e4d3bc73dacd659e96b524f881d28f3862
nitamigina <61388805+nitamigina@users.noreply.github.com>
2022-04-25 14:03:25 +0700

    Update mobo.sh

--- mobo.sh
+++ mobo.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-#
-# Choose nearest stratum:
-#       stratum-ru.rplant.xyz   /Moscow/
-#       stratum-eu.rplant.xyz   /London/
-#       stratum-asia.rplant.xyz /Singapore/
-#       stratum-na.rplant.xyz   /Toronto/
-#
-FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
-"$FOLDER"/cpuminer-sse2 -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u WALLET.WORKER_NAME
-sleep 5
+	./mobo -a minotaurx  -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RTbYX9JPTRv9xn8JSSdWdwt3wnpnexUp9q.mozavn1
+	sleep 5
 done
